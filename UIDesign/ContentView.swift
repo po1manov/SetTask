929779f2beab37ebd66cb67bd25ddca343e81daf
po1manov <poimanov.egor@gmail.com>
2024-02-26 15:53:25 -0500

    Initial Commit

---
+++ UIDesign/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  UIDesign
+//
+//  Created by Egor Poimanov on 2024-02-26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ UIDesign/UIDesignApp.swift
@@ -0,0 +1,17 @@
+//
+//  UIDesignApp.swift
+//  UIDesign
+//
+//  Created by Egor Poimanov on 2024-02-26.
+//
+
+import SwiftUI
+
+@main
+struct UIDesignApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
